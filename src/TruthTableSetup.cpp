a919ade63e1468a5a0b8483024bbbe78078d8466
albrdev <albrdev@gmail.com>
2022-02-27 03:56:37 +0100

    Minor fix

--- src/TruthTableSetup.cpp
+++ src/TruthTableSetup.cpp
@@ -211,6 +211,7 @@ void InitTruthTable(TruthTableExpressionParser& instance)
   addVariable(false, "false", "False", "Boolean value");
   addVariable(false, "f", "False", "Boolean value");
   addVariable(false, "F", "False", "Boolean value");
+  defaultVariableInfoMap.push_back(std::make_tuple(nullptr, "", ""));
   addVariable(true, "high", "High", "Boolean value");
   addVariable(true, "h", "High", "Boolean value");
   addVariable(true, "H", "High", "Boolean value");
