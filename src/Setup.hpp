f367e65def6d9f25e1c4f69abeab7cfbc644f718
albrdev <albrdev@gmail.com>
2022-03-03 22:12:10 +0100

    Updated submodules: Added namespaces

--- src/Setup.hpp
+++ src/Setup.hpp
@@ -10,22 +10,22 @@
 #include "text/exception/SyntaxException.hpp"
 
 using DefaultArithmeticType = bool;
-using DefaultValueType      = text::expression::ValueToken<std::nullptr_t, DefaultArithmeticType>;
-using DefaultVariableType   = text::expression::VariableToken<std::nullptr_t, DefaultArithmeticType>;
+using DefaultValueType      = Text::Expression::ValueToken<std::nullptr_t, DefaultArithmeticType>;
+using DefaultVariableType   = Text::Expression::VariableToken<std::nullptr_t, DefaultArithmeticType>;
 
-using text::expression::IValueToken;
-using text::expression::IVariableToken;
-using text::expression::IUnaryOperatorToken;
-using text::expression::IBinaryOperatorToken;
-using text::expression::IFunctionToken;
-using text::expression::UnaryOperatorToken;
-using text::expression::BinaryOperatorToken;
-using text::expression::FunctionToken;
+using Text::Expression::IValueToken;
+using Text::Expression::IVariableToken;
+using Text::Expression::IUnaryOperatorToken;
+using Text::Expression::IBinaryOperatorToken;
+using Text::Expression::IFunctionToken;
+using Text::Expression::UnaryOperatorToken;
+using Text::Expression::BinaryOperatorToken;
+using Text::Expression::FunctionToken;
 
-using text::expression::Associativity;
+using Text::Expression::Associativity;
 
-using text::expression::ExpressionParserBase;
-using text::exception::SyntaxException;
+using Text::Expression::ExpressionParserBase;
+using Text::Exception::SyntaxException;
 
 inline std::unordered_map<char, std::unique_ptr<UnaryOperatorToken>> defaultUnaryOperatorCache;
 inline std::unordered_map<char, IUnaryOperatorToken*> defaultUnaryOperators;
